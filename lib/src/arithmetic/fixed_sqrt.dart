d4e9fe1884837a1e626b4114208c739c29360bfe
James Farwell <jcfarwe@flex-alpha-rohd.ch.intel.com>
2025-03-04 00:48:57 +0000

    Create FixedSqrt base class, add to arithmetic.dart

---
+++ lib/src/arithmetic/fixed_sqrt.dart
@@ -0,0 +1,39 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Indentifier: BSD-3-Clause
+//
+// fixed_point_sqrt.dart
+// An abstract base class defining the API for floating-point square root.
+//
+// 2025 March 3
+// Authors: James Farwell <james.c.farwell@intel.com>, Stephen
+// Weeks <stephen.weeks@intel.com>
+
+/// An abstract API for fixed point square root.
+import 'package:meta/meta.dart';
+import 'package:rohd/rohd.dart';
+import 'package:rohd_hcl/rohd_hcl.dart';
+
+abstract class FixedPointSqrt<FpType extends FloatingPoint> extends Module {
+  /// Width of the output exponent field.
+  final int numWidth;
+
+  /// The value [a], named this way to allow for a local variable 'a'.
+  @protected
+  late final Logic a;
+
+  /// getter for the computed [FloatingPoint] output.
+  late final Logic sqrt = a.clone(name: 'sqrt')..gets(output('sqrt'));
+
+  /// Square root a fixed point number [a], returning result in [sqrt].
+  FixedPointSqrt(Logic a,
+      {super.name = 'fixed_point_square_root', String? definitionName})
+      : numWidth = a.width,
+        super(
+            definitionName:
+                definitionName ?? 'FixedPointSquareRoot_E${a.width}') {
+    this.a = (a.clone(name: 'a') as FpType)
+      ..gets(addInput('a', a, width: a.width));
+
+    addOutput('sqrt', width: numWidth);
+  }
+}
